2abca0ba69f7a4a0bf2da145ceb2ecec87964a65
Nather_ / Nathan Delenclos <nathan.delenclos@epitech.eu>
2024-11-09 01:43:02 +0100

    feat: set branding of storybook

---
+++ .storybook/FirstBreath.ts
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming';
+
+export default create({
+    base: 'dark',
+    brandTitle: 'FirstBreath',
+    brandUrl: 'https://firstbreath.github.io/design-system/',
+    brandImage: 'https://media.discordapp.net/attachments/1286951938646081610/1304487568548692020/Firstbreath-logo-clean.png?ex=672f9238&is=672e40b8&hm=e673a462b0137e0e72e133c5dc45a251cbddedd5c0edcbaaa31a3922c8fba425&=&format=webp&quality=lossless',
+    brandTarget: '_self',
+});
