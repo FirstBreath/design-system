568b4bc7358440076d513053f07bfc325cc0bca9
Nathandelenclos <Nathandelenclos@users.noreply.github.com>
2024-11-20 23:26:21 +0000

    deploy: 2ad5246ec2c6aafdd4c4e2c51c4c0a5284e94ad4

---
+++ assets/Card.component-ChmowwiA.js
@@ -0,0 +1,7 @@
+import{R as r}from"./index-DJO9vBfz.js";import{d as a}from"./styled-components.browser.esm-BjS_sxvV.js";const d=a.div`
+    padding: 24px;
+    gap: 40px;
+
+    border-radius: ${({theme:e})=>e.borderRadius};
+    background: ${({theme:e})=>e.colors.surface};
+`,o=({children:e})=>r.createElement(d,null,e);o.__docgenInfo={description:"",methods:[],displayName:"Card",props:{children:{required:!0,tsType:{name:"ReactReactNode",raw:"React.ReactNode"},description:""}}};export{o as C};
